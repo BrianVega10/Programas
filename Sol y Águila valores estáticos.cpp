77a5a2b41e9f8565c4440bdd68caf761d7f911a2
BrianVega10 <88786103+BrianVega10@users.noreply.github.com>
2021-09-10 15:21:06 -0500

    Create Sol y Águila valores estáticos.cpp

---
+++ Sol y Águila valores estáticos.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+	int a,v,x=0;
+	v=a*2;
+	x=rand()%2;
+	if(x==0)
+	printf("Usted gano el doble:%d",v);
+    else
+	printf("Usted perdio la apuesta");
+	
+}
