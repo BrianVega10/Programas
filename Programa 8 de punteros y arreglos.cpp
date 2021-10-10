fbeb18913f72d90eb1ac60a92a4b110364a71f32
BrianVega10 <88786103+BrianVega10@users.noreply.github.com>
2021-10-10 18:22:52 -0500

    Create Programa 8 de punteros y arreglos.cpp

---
+++ Programa 8 de punteros y arreglos.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/* eco de los argumentos de la línea de órdenes; 2a. versión */
+main(int argc, char *argv[ ])
+{
+while (— argc > 0)
+printf("%s%s", *+ + argv, (argc > 1) ? “ "
+printf("");
+retum 0;
+}
