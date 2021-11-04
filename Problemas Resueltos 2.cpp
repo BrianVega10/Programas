73d296fc33362e3af9f7c1004b7d76c1390ae92a
BrianVega10 <88786103+BrianVega10@users.noreply.github.com>
2021-11-04 16:43:05 -0600

    Create Problemas Resueltos 2.cpp

---
+++ Problemas Resueltos 2.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+main()
+{
+int a, b=0;
+a=b*b+1;
+printf ("Si b=%d, entonces a=%d\n", b, a);
+b=2;
+printf ("Si b=%d, entonces a=%d\n", b, a);
+b=-2;
+printf ("Si b=%d, entonces a=%d\n", b, a);
+} 
